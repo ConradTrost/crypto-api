198d65896a65cd7d49a8b720c342b7e2cedf8b7b
Conrad Trost <conradtrost@Conrads-MBP.lan>
2022-02-19 16:53:01 -0600

    Create directories if not there

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from constants import API_KEY
 
 class Coin():
@@ -10,6 +11,7 @@ class Coin():
         self.ticker = ticker
 
     def get_data(self, fileName, url):
+        print(f'\nRequesting data from {url}..')
         response = requests.get(url)
         self.write_file(fileName, response.json())
 
@@ -17,22 +19,29 @@ class Coin():
         return f'{self.base_url}?{endpoint}&apikey={self.key}'
 
     def write_file(self, fileName, data):
-        f = open(f'{fileName}/{self.ticker}to{self.market}.json', "w")
+        f = open(fileName, "w")
         f.write(json.dumps(data))
         f.close()
 
-    def find_data(self, fileName, url):
+    def find_data(self, directory, url):
+        fileName = f'{directory}/{self.ticker}to{self.market}.json'
+
         while True:
             try:
-                f = open(f'{fileName}/{self.ticker}to{self.market}.json', 'r')
+                f = open(fileName, 'r')
+                print(f'\nReading data from current {fileName}...')
                 return json.load(f)
             except IOError: 
+                try:
+                    os.mkdir(directory)
+                except OSError as error: 
+                    print(error)  
                 self.get_data(fileName, url)
 
     # Intervals can be 5, 10, 15, 30, 60 for intraday
     def intraday(self, interval):
         endpoint = f'function=CRYPTO_INTRADAY&symbol={self.ticker}&market={self.market}&interval={interval}min&outputsize=full'
-        self.find_data(f'intraday/{interval}', self.url(endpoint))
+        self.find_data(f'_data/intraday/{interval}', self.url(endpoint))
 
 
     def currentExchangeRate(self):
@@ -41,13 +50,10 @@ class Coin():
 
     def daily(self):
         endpoint = f'function=DIGITAL_CURRENCY_DAILY&symbol={self.ticker}'
-        self.find_data(f'daily', self.url(endpoint))
+        self.find_data('_data/daily', self.url(endpoint))
 
 
 btc = Coin('BTC', 'USD')
 
-print('')
-# btc.intraday(60)
-# btc.currentExchangeRate()
-# print('')
+btc.intraday(5)
 btc.daily()
